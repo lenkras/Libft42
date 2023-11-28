3b1c2b72ce0dd67051c0752a7a019bb481b34e18
Elena Polkhovski <epolkhov@c2r3p11.hive.fi>
2023-11-28 20:55:15 +0200

    add new functions

---
+++ ft_substr.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: epolkhov <epolkhov@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/02 10:06:44 by epolkhov          #+#    #+#             */
+/*   Updated: 2023/11/08 18:31:34 by epolkhov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/*
+ * Allocates (with malloc(3)) and returns a substring
+ * from the string ’s’.
+ * The substring begins at index ’start’ and is of
+ * maximum size ’len’
+ * .
+*/
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*substr;
+
+	if (!s)
+		return (NULL);
+	if (start > ft_strlen(s))
+	{
+		substr = malloc(1);
+		if (!substr)
+			return (NULL);
+		substr[0] = '\0';
+		return (substr);
+	}
+	if (len > ft_strlen(s) - start)
+		len = ft_strlen(s) - start;
+	substr = (char *)malloc(sizeof(char) * (len + 1));
+	if (substr == NULL)
+		return (NULL);
+	ft_strlcpy(substr, s + start, len + 1);
+	return (substr);
+}
